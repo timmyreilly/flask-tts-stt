d249febbde5fa0a375410ee0c9bc9f5f9abb2522
timmyreilly <timreilly@live.com>
2016-01-06 12:40:23 -0800

    trying to recreate

--- tts_helper.py
+++ tts_helper.py
@@ -1,1 +1,9 @@
 from tokens import * 
+
+SPEECH_END_POINT = "https://speech.platform.bing.com/recognize"
+
+ttsHost = "https://speech.platform.bing.com"
+
+payload = {'grant_type': 'client_credentials', 'client_id': 'nothing', 'client_secret': OXFORD_SPEECH_API, 'scope': ttsHost}
+
+headers = {"Content-type": "application/x-www-form-urlencoded"}
